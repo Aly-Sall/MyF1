de7b877a55d6ff2db46ec43769d4553be5d4ceeb
Aly-Sall <alysall2001@gmail.com>
2024-12-30 03:56:05 +0100

    update drivers

--- src/pages/drivers/driver/DriverDetail.tsx
+++ src/pages/drivers/driver/DriverDetail.tsx
@@ -9,9 +9,9 @@ interface Driver {
   img: string;
   givenName: string;
   familyName: string;
-  nationality: string;
+  nationality?: string;
   flag: string;
-  team: string;
+  team?: string;
   grandsPrixEntered?: number;
   worldChampionships?: string;
   highestRaceFinish?: string;
@@ -20,15 +20,16 @@ interface Driver {
   placeOfBirth?: string;
   helmet?: string;
   img2?: string;
+  team_logo?: string;
 }
 
 const DriverDetail: React.FC = () => {
   const { familyName } = useParams<{ familyName: string }>();
-  const driver: Driver | undefined = driversData.find(
+  const driver: Driver | undefined = driversData?.find(
     (d) => d.familyName === familyName
   );
 
-  if (!driver) {
+  if (!DriverDetail) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen text-center">
         <h2 className="text-2xl font-bold text-red-600">Pilote non trouvé</h2>
@@ -43,20 +44,20 @@ const DriverDetail: React.FC = () => {
     <div className="lg:flex p-8">
       <div className="lg:w-1/2">
         <img
-          src={driver.img2 || "/placeholder-driver.jpg"}
-          alt={`${driver.givenName} ${driver.familyName}`}
+          src={driver?.img2 || "/placeholder-driver.jpg"}
+          alt={`${driver?.givenName} ${driver?.familyName}`}
           className="w-full h-auto mb-9"
         />
         <div className="flex items-center ml-4 space-x-4">
           <img
-            src={driver.permanentNumber || "/placeholder-number.png"}
+            src={driver?.permanentNumber || "/placeholder-number.png"}
             alt="Permanent Number"
             height={60}
             width={60}
           />
           <img
-            src={driver.flag || "/placeholder-flag.png"}
-            alt={`${driver.nationality} Flag`}
+            src={driver?.flag || "/placeholder-flag.png"}
+            alt={`${driver?.nationality} Flag`}
             height={60}
             width={60}
           />
@@ -64,8 +65,8 @@ const DriverDetail: React.FC = () => {
       </div>
 
       <div className="lg:w-1/2 lg:ml-8">
-        <h2 className="text-3xl font-bold mb-4">{`${driver.givenName} ${driver.familyName}`}</h2>
-        {driver.helmet && (
+        <h2 className="text-3xl font-bold mb-4">{`${driver?.givenName} ${driver?.familyName}`}</h2>
+        {driver?.helmet && (
           <img
             src={driver.helmet}
             alt={`${driver.givenName}'s Helmet`}
@@ -76,14 +77,14 @@ const DriverDetail: React.FC = () => {
           <table className="min-w-full border-collapse border border-gray-300">
             <tbody>
               {[
-                ["Team", driver.team],
-                ["Country", driver.nationality],
-                ["Grands Prix entered", driver.grandsPrixEntered],
-                ["World Championships", driver.worldChampionships],
-                ["Highest race finish", driver.highestRaceFinish],
-                ["Highest grid position", driver.highestGridPosition],
-                ["Date of birth", driver.dateOfBirth],
-                ["Place of birth", driver.placeOfBirth],
+                ["Team", driver?.team],
+                ["Country", driver?.nationality],
+                ["Grands Prix entered", driver?.grandsPrixEntered],
+                ["World Championships", driver?.worldChampionships],
+                ["Highest race finish", driver?.highestRaceFinish],
+                ["Highest grid position", driver?.highestGridPosition],
+                ["Date of birth", driver?.dateOfBirth],
+                ["Place of birth", driver?.placeOfBirth],
               ].map(
                 ([label, value]) =>
                   value && (
